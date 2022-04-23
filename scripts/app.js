1e813a027f3a2baea43a4cbbafff72a343530e2a
Paulo Henrique <paulo.henrique1080p@gmail.com>
2022-04-23 10:34:20 -0300

    closes #3; 1º: refactor: app.js.

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,4 @@
 
-// const btnCalculate = document.querySelector(".handle__main__btn");
-
-
-
-
-// btnCalculate.addEventListener('click', () => {
-    
-//     const valueCalc = document.querySelector(".handle__main__input").value;
-
-//     Number(valueCalc);
-//     var a = parseFloat(valueCalc);
-//     console.log(typeof(a));
-
-
-
-//     console.log(valueCalc);
-
-// })
-
-
-// let hasValueComputed = false;
-
 function insert(newValue, isOperator){
 
     // aqui abaixo não funcionou a verificacao se o operador é ( / || * || - ), parece que é bug do JS...
@@ -28,32 +6,13 @@ function insert(newValue, isOperator){
     // ...quando o valor for operador matematico ex: "+, -, / e * "
 
     if(isOperator){
-        // alert('ola')
-    }
-
-    console.log(isOperator);
-    console.log(typeof(newValue));
-
-    // newValue.toString()
-
-    const value3 = newValue;
-
-    const barra = '/';
-    const vezes = '*'
-
-    if(isOperator){
 
         document.getElementById("calculator__output__previus-operand").innerHTML = "";
-        
-    }
+    }    
 
-    else if(isOperator === undefined){
-
-        
-        // document.getElementById("calculator__output__current-operand").innerHTML = "";
+    else{
 
         const calculatorOutputPrevius = document.getElementById("calculator__output__previus-operand").innerHTML;
-
 
         if(calculatorOutputPrevius){
             
@@ -61,19 +20,7 @@ function insert(newValue, isOperator){
             document.getElementById("calculator__output__previus-operand").innerHTML = "";
 
         }
-        // if (calculatorOutputPrevius.length > 0) {
-        // // alert("CLEAR");
-
-        //     document.getElementById("calculator__output__previus-operand").innerHTML = "";
-
-        //     document.getElementById("calculator__output__current-operand").innerHTML = "";
-        // }
-
     }
-
-
-
-
 
     const valueAlreadyComputed = document.getElementById("calculator__output__current-operand").innerHTML;
 
@@ -81,8 +28,8 @@ function insert(newValue, isOperator){
 
     document.getElementById("calculator__output__current-operand").innerHTML = newValueComputed;
 
-    // console.log(newValueComputed);
 }
+
 
 function clearCalculator(){
 
@@ -97,24 +44,10 @@ function calculate(){
 
     const valueAlreadyComputed = document.getElementById("calculator__output__current-operand").innerHTML;
 
-    // console.log(valueAlreadyComputed.length)
-
-
-    // valueAlreadyComputed.length > 0;
-
-    // hasValueComputed = true;
-
 
     document.getElementById("calculator__output__previus-operand").innerHTML = valueAlreadyComputed;
 
     document.getElementById("calculator__output__current-operand").innerHTML = eval(valueAlreadyComputed);
-
-    // hasValueComputed = false;
-
-
-        // document.getElementById("calculator__output__current-operand").innerHTML = eval(valueAlreadyComputed);
-
-
 
     if(valueAlreadyComputed.typeOf ){
 
